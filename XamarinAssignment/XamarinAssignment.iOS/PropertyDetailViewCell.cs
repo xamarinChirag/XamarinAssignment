1c3019ed4435b25a0571e33e358e1159c9c583f5
chirag.surti <CSSURTI01@CORP>
2016-04-16 04:32:40 +0530

    storyboard changes

---
+++ XamarinAssignment/XamarinAssignment.iOS/PropertyDetailViewCell.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace XamarinAssignment.iOS
+{
+	partial class PropertyDetailViewCell : UITableViewCell
+	{
+		public PropertyDetailViewCell (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
